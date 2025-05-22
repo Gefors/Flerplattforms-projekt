cc22324c992bb1d9ba529d67aa48527f965f3ea2
timspel <timdo2003@gmail.com>
2025-05-22 10:01:20 +0200

    Added styling to WorkoutForm, removed elements
    Used tailwind to style the WorkoutForm and also removing previous elements that were used as testing.

--- src/App.tsx
+++ src/App.tsx
@@ -15,32 +15,29 @@ function App() {
 
   return (
     // <>
-    <div className="mx-20 flex flex-col">
-      {showAlert && (
-        <Alert
-          text="Your workout have been saved"
-          className="flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3"
-        ></Alert>
-      )}
-      <h1 className="text-cyan-950">Welcome to GymTime</h1>
-      <p>Form for adding workout</p>
-      <WorkoutForm />
-      <Button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-20"
-        onClick={() => console.log("Clicked")}
-      >
-        Search
-      </Button>
-      <hr />
-      <p>Display workout</p>
-      <Button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-35 "
-        onClick={() => setShowAlert(true)}
-      >
-        Save workout
-      </Button>
+    <div className="bg-gray-50">
+    
+    
+    <div className="container mx-auto ">
+      <div className=" flex flex-col">
+        <WorkoutForm />
+        <Button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-20"
+          onClick={() => console.log("Clicked")}
+        >
+          Search
+        </Button>
+        <hr />
+        <p>Display workout</p>
+        <Button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-35 "
+          onClick={() => setShowAlert(true)}
+        >
+          Save workout
+        </Button>
+      </div>
     </div>
-    //  </>
+    </div>
   );
 }
 
--- src/components/WorkoutForm.tsx
+++ src/components/WorkoutForm.tsx
@@ -82,102 +82,108 @@ const WorkoutForm: React.FC = () => {
   ];
 
   return (
-    <form action=""
-      className="mx-auto bg-white rounded-xl overflow-hidden w-full ">
+    <div className="rounded-xl overflow-hidden w-full mt-10">
+      <div className="bg-gradient-to-r from-violet-500 to-blue-500 px-8 py-6">
+        <h2 className="text-white text-xl font-bold">Create your workout</h2>
+      </div>
+      <form action="" className="mx-auto bg-white  overflow-hidden w-full p-8">
         {/* Goal */}
-        <div className="bg-gradient-to-r from-blue-600 to-purple-400 px-8 py-6">
-            <h2 className="text-white text-xl font-bold">Create your workout</h2>
-        </div>
-      <div>
-        <label className="block font-medium">Goal</label>
-        <select
-          name="goal"
-          id=""
-          className="w-full border rounded px-3 py-2"
-          onChange={handeSelectChange}
-          value={form.goal}
-        >
-          <option value="strength">Strength</option>
-          <option value="weight-loss">Weight Loss</option>
-          <option value="muscle-growth">Muscle Growth</option>
-        </select>
-      </div>
-
-      {/*Experience  */}
-      <div>
-        <label className="">Experience</label>
-        <select
-          name="experience"
-          id=""
-          className="w-full border rounded px-3 py-2"
-          onChange={handeSelectChange}
-          value={form.experience}
-        >
-          <option value="beginner">Beginner</option>
-          <option value="intermediate">Intermediate</option>
-          <option value="advanced">Advanced</option>
-        </select>
-      </div>
-
-      {/*Duratioon */}  
-      <div>
-        <label className="">Duration</label>
-        <select
-          name="duration"
-          id=""
-          className="w-full border rounded px-3 py-2"
-          onChange={handeSelectChange}
-          value={form.duration}
-        >
-          <option value="short">Short</option>
-          <option value="medium">Medium</option>
-          <option value="long">Long</option>
-        </select>
-      </div>
-
-    <div className=" flex flex-col md:flex-row justify-between gap-16 my-10 p-4 border rounded-lg w-84">
-        {/* Equipment */}
-      <div className="flex flex-col gap-4">
-        <label>Equipment</label>
         <div>
-          {equipmentGroupOptions.map((equipment) => (
-            <div key={equipment} className="flex flex-row items-center gap-2 w-full">
-              <input
-                type="checkbox"
-                checked={form.equipment.includes(equipment)}
-                name="equipment"
-                id=""
-                value={equipment}
-                onChange={handleEquipmentChange}
-              />
-              <label>{equipment}</label>
-            </div>
-          ))}
+          <label className="block font-medium">Goal</label>
+          <select
+            name="goal"
+            id=""
+            className="w-full border rounded px-3 py-2 "
+            onChange={handeSelectChange}
+            value={form.goal}
+          >
+            <option value="strength">Strength</option>
+            <option value="weight-loss">Weight Loss</option>
+            <option value="muscle-growth">Muscle Growth</option>
+          </select>
         </div>
-      </div>
 
-      {/*Muscle Groups  */}
-      <div className="flex flex-col gap-4">
-        <label className="text-nowrap">Muscle Groups</label>
-        <div className="h-full">
-          {muscleGroupOptions.map((muscle) => (
-            <div key={muscle} className="flex flex-row items-center gap-2 w-full">
-              <input
-                type="checkbox"
-                checked={form.muscleGroup.includes(muscle)}
-                name="muscleGroup"
-                id=""
-                value={muscle}
-                onChange={handleMuscleGroupChange}
-              />
-              <label>{muscle}</label>
-            </div>
-          ))}
+        {/*Experience  */}
+        <div>
+          <label className="">Experience</label>
+          <select
+            name="experience"
+            id=""
+            className="w-full border rounded px-3 py-2"
+            onChange={handeSelectChange}
+            value={form.experience}
+          >
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="advanced">Advanced</option>
+          </select>
         </div>
-      </div>
-    </div>
-      
-      <div>
+
+        {/*Duratioon */}
+        <div>
+          <label className="">Duration</label>
+          <select
+            name="duration"
+            id=""
+            className="w-full border rounded px-3 py-2"
+            onChange={handeSelectChange}
+            value={form.duration}
+          >
+            <option value="short">Short</option>
+            <option value="medium">Medium</option>
+            <option value="long">Long</option>
+          </select>
+        </div>
+
+        <div className=" flex flex-col md:flex-row justify-between gap-16 my-10 p-4 border rounded-lg w-84 ">
+          {/* Equipment */}
+          <div className="flex flex-col gap-4">
+            <label>Equipment</label>
+            <div>
+              {equipmentGroupOptions.map((equipment) => (
+                <div
+                  key={equipment}
+                  className="flex flex-row items-center gap-2 w-full"
+                >
+                  <input
+                    type="checkbox"
+                    checked={form.equipment.includes(equipment)}
+                    name="equipment"
+                    id=""
+                    value={equipment}
+                    onChange={handleEquipmentChange}
+                  />
+                  <label>{equipment}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/*Muscle Groups  */}
+          <div className="flex flex-col gap-4">
+            <label className="text-nowrap">Muscle Groups</label>
+            <div className="h-full">
+              {muscleGroupOptions.map((muscle) => (
+                <div
+                  key={muscle}
+                  className="flex flex-row items-center gap-2 w-full"
+                >
+                  <input
+                    type="checkbox"
+                    checked={form.muscleGroup.includes(muscle)}
+                    name="muscleGroup"
+                    id=""
+                    value={muscle}
+                    onChange={handleMuscleGroupChange}
+                  />
+                  <label>{muscle}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div>
           <button
             className="p-3 bg-blue-500 hover:cursor-pointer text-white"
             onClick={submitForm}
@@ -185,7 +191,8 @@ const WorkoutForm: React.FC = () => {
             Submit!
           </button>
         </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
