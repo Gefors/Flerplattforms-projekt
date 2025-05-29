06ed8989256c966c7ed1b7eff1b64d4e6c62848d
Alxflem <fleming.alexander99@gmail.com>
2025-05-29 13:11:29 +0200

    Update ContentContainer.tsx
    Added some margin at the bottom.

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -4,7 +4,7 @@ interface ContentContainerProps {
 
 function ContentContainer({ children }: ContentContainerProps) {
     return (
-        <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <div className="container mb-10 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
             {children}
         </div>
     );
